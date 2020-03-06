1d91a8d1ba79d50e6c2291ac7f6eaba66b9628df
Giovanni SIMON <giovanni.simon58@gmail.com>
2020-03-06 09:23:30 +0100

    methodes interface

--- src/interfaces/Igestion.java
+++ src/interfaces/Igestion.java
@@ -1,5 +1,18 @@
 package interfaces;
 
+import metier.Etudiant;
+
 public interface Igestion {
 
+	abstract Etudiant creerEtudiant (int idEtudiant, String nomEtudiant, String prenomEtudiant, String mailEtudiant,
+	String adresseEtudiant, String telephoneEtudiant, String dateNaissanceEtudiant);
+	
+	abstract void lireEtudiant(int idEtudiant);
+	
+	abstract void supprimerEtudiant(int idEtudiant);
+	
+	abstract void modifierAdresseEtudiant(int idEtudiant,String nouvelleAdresse);
+	
+	abstract void associerCoursEtudiant(int idEtudiant,String theme);
+	
 }
